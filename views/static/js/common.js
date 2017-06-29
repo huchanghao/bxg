8371f662fa064ad0723f45237f853eab06cae818
胡长昊 <hao13062770368@163.com>
2017-06-29 20:17:46 +0800

    增加了注释

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,11 +1,10 @@
-
+// 模块化的开始  引入模块化的依赖文件   最后回调函数 接受 依赖文件的产出的结果当做形参，传入函数中
+								    // (一定注意形参的顺序。要和依赖文件产出的结果顺序一致）
 define(["jquery","template","cookie"],function($,template){
 	// template 渲染的功能
 	var inform = JSON.parse($.cookie("inform"));
 	var strtpl = template("tpl",inform);
 	$("#infrom").html(strtpl);
-
-
 //       退出功能呢
 	$("#quit").click(function(){
 		$.ajax({
